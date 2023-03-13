71bacb521cda722bc243dd1be2c73612706f4126
Jigsaw28 <levenko7@gmail.com>
2023-03-13 22:37:50 +0200

    complete

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,23 +1,20 @@
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 // Add imports above this line
 import { galleryItems } from './gallery-items';
-import SimpleLightbox from "simplelightbox";
 // Change code below this line
 
-console.log(galleryItems);
-
-const gallery = document.querySelector("ul.gallery");
+const gallery = document.querySelector(".gallery");
 
 const markup = galleryItems
   .map((item) => {
     // const { original, preview, description } = item;
-    return `<li class="list-item">
-  <a class="gallery__link" href="${item.original}">
+    return `<a class="gallery__link" href="${item.original}">
   <img class="gallery__image" src="${item.preview}" alt="${item.description}" />
-</a>
-</li>`;
+</a>`;
   })
   .join("");
-gallery.insertAdjacentHTML("afterbegin", markup);
+gallery.insertAdjacentHTML("beforeend", markup);
 
 const galleryLightBox = new SimpleLightbox(".gallery a", {
   captionsData: "alt",
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,36 @@
+import throttle from "lodash.throttle";
 
+const form = document.querySelector('.feedback-form');
+const email = document.querySelector('input[name = "email"]');
+const message = document.querySelector('textarea[name = "message"]')
+
+form.addEventListener('submit', onFormSubmit);
+form.addEventListener('input', throttle(onTextareaInput, 500));
+
+const dataStorage = {}
+
+function onFormSubmit(event) {
+    event.preventDefault();
+    event.currentTarget.reset();
+    localStorage.removeItem("feedback-form-state")
+    console.log(dataStorage)
+}
+
+function onTextareaInput() {
+    dataStorage.email = email.value;
+    dataStorage.message = message.value;
+
+    localStorage.setItem("feedback-form-state", JSON.stringify(dataStorage))   
+}
+
+storageStatus()
+
+function storageStatus() {
+    const saveData = JSON.parse(localStorage.getItem("feedback-form-state"));
+    if (saveData) {
+        email.value = saveData.email;
+        message.value = saveData.message
+    }
+    }
+
+    
